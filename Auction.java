c772b07ca78e645bdb8682340ff2601471355736
Denzel <kiandenzel@yahoo.com>
2026-03-22 13:32:05 -0400

    Question 6

--- Auction.java
+++ Auction.java
@@ -85,27 +85,16 @@ public class Auction
      */
     public Lot getLot(int lotNumber)
     {
-        if((lotNumber >= 1) && (lotNumber < nextLotNumber)) {
-            // The number seems to be reasonable.
-            Lot selectedLot = listOfLots.get(lotNumber - 1);
-            // Include a confidence check to be sure we have the
-            // right lot.
-            if(selectedLot.getNumber() != lotNumber) {
-                System.out.println("Internal error: Lot number " +
-                                   selectedLot.getNumber() +
-                                   " was returned instead of " +
-                                   lotNumber);
-                // Don't return an invalid lot.
-                selectedLot = null;
+        for(Lot lot : listOfLots){
+            if(lot.getNumber()==lotNumber){
+                return lot;
             }
-            return selectedLot;
         }
-        else {
             System.out.println("Lot number: " + lotNumber +
                                " does not exist.");
             return null;
-        }
     }
+    
     
     public void close(){
         for(Lot lot : listOfLots){
